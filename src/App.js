9157d91471d578b10879a3e5d8accf800355af35
manuCastrillonM <10585946+ManuCastrillonM@users.noreply.github.com>
2025-04-28 22:51:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import jokes from './assets/jokes.json';
 
 function App() {
+  const [joke, setJoke] = useState('');
+
+  const getRandomJoke = () => {
+    const randomIndex = Math.floor(Math.random() * jokes.jokes.length);
+    return jokes.jokes[randomIndex];
+  };
+
+  useEffect(() => {
+    setJoke(getRandomJoke());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1>BAD JOKES GENERATOR</h1>
+        <div className="joke-card">
+          {joke.split('?').length > 1 ? (
+            <>
+              <p className="question">{joke.split('?')[0]}?</p>
+              <p className="answer">{joke.split('?')[1]}</p>
+            </>
+          ) : (
+            <p className="single-line">{joke}</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
